d326219532fafc81fb655dc1e34a48880217ce7c
btclark1 <73087915+btclark1@users.noreply.github.com>
2021-05-14 11:11:05 -0400

    Add back in printf's

--- boards/som_mx8mn/multicore_examples/rpmsg_lite_str_echo_rtos/main_remote.c
+++ boards/som_mx8mn/multicore_examples/rpmsg_lite_str_echo_rtos/main_remote.c
@@ -98,12 +98,12 @@ static void app_task(void *param)
         app_buf[len] = 0; /* End string by '\0' */
 
      /* BTC - Remove printf's for throughput testing */
-        /*
+        
         if ((len == 2) && (app_buf[0] == 0xd) && (app_buf[1] == 0xa))
             PRINTF("Get New Line From Master Side\r\n");
         else
             PRINTF("Get Message From Master Side : \"%s\" [len : %d]\r\n", app_buf, len);
-        */
+        
         
         /* Get tx buffer from RPMsg */
         tx_buf = rpmsg_lite_alloc_tx_buffer(my_rpmsg, &size, RL_BLOCK);
