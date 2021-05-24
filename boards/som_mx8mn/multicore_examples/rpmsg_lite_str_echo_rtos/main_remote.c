b1b1ca3c0ac0979aa2437cb476ce3a4bdb1baffc
btclark1 <73087915+btclark1@users.noreply.github.com>
2021-05-24 08:57:45 -0400

    Update main_remote.c
    Commented out printf's

--- boards/som_mx8mn/multicore_examples/rpmsg_lite_str_echo_rtos/main_remote.c
+++ boards/som_mx8mn/multicore_examples/rpmsg_lite_str_echo_rtos/main_remote.c
@@ -99,22 +99,22 @@ static void app_task(void *param)
         memcpy(app_buf, rx_buf, len);
 
         /* BTC - print app_buf before its cleared when len is 0  */
-        if(len == 0)
+        /*if(len == 0)
            PRINTF("Get Message From Master Side...len is 0 .. BTC  : \"%s\" [len : %d]\r\n", app_buf, len);
+        */
 
         app_buf[len] = 0; /* End string by '\0' */
 
         /* BTC Remove printfs so they are not included in throughput test */
-        
+        /*
         if ((len == 2) && (app_buf[0] == 0xd) && (app_buf[1] == 0xa))
             PRINTF("Get New Line From Master Side...BTC \r\n");
         else
             PRINTF("Get Message From Master Side...BTC  : \"%s\" [len : %d]\r\n", app_buf, len);
-        
+        */
 
         /* Get tx buffer from RPMsg */
         tx_buf = rpmsg_lite_alloc_tx_buffer(my_rpmsg, &size, RL_BLOCK);
-        PRINTF("\nrpmsg_lite_alloc_tx_buffer, size is = %d, RL_BLOCK = %d \r\n\n", size, RL_BLOCK);
         
         assert(tx_buf);
         /* Copy string to RPMsg tx buffer */
