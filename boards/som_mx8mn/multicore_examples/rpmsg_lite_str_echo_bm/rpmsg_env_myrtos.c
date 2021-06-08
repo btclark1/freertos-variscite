8a7711495d0f8aef4e36f67e9ba9898c5bd3d2f2
BTC <briantclark@gmail.com>
2021-06-08 11:59:58 -0400

    fix bugs

--- boards/som_mx8mn/multicore_examples/rpmsg_lite_str_echo_bm/rpmsg_env_myrtos.c
+++ boards/som_mx8mn/multicore_examples/rpmsg_lite_str_echo_bm/rpmsg_env_myrtos.c
@@ -167,8 +167,7 @@ int main(void)
         len = rx_msg[rx_idx].len;
         if (len > sizeof(app_buf))
         {
-            printf(" Length of message recieved from A53 larger than M7 app_buf buffer, chopping to sizeof(app_buf) = %d and sending back",
-                             sizeof(app_buf);
+            printf(" Length of message recieved from A53 larger than M7 app_buf buffer, chopping to sizeof(app_buf) = %d and sending back",sizeof(app_buf));
             len = sizeof(app_buf);
         }
         /* Copy string from RPMsg rx buffer */
