22bff8b956fe0f8ae5862b46219af9373e010074
BTC <briantclark@gmail.com>
2021-06-08 21:19:28 -0400

    fix bug in rtos version

--- boards/som_mx8mn/multicore_examples/rpmsg_lite_str_echo_rtos/main_remote.c
+++ boards/som_mx8mn/multicore_examples/rpmsg_lite_str_echo_rtos/main_remote.c
@@ -134,13 +134,6 @@ static void app_task(void *param)
             {
                 assert(false);
             }
-            /* Release held RPMsg rx buffer */
-            result = rpmsg_queue_nocopy_free(my_rpmsg, rx_buf);
-            if (result != 0)
-            {
-                assert(false);
-            }
-
             /* BTC Remove printfs so they are not included in throughput test */        
             if ((len == 2) && (app_buf[0] == 0xd) && (app_buf[1] == 0xa))
                 PRINTF("Get New Line From Master Side...RTOS \r\n");
@@ -148,7 +141,12 @@ static void app_task(void *param)
                 PRINTF("Get Message From Master Side...RTOS.. : [len : %d], size = %d, byte_cnt = %d\r\n",
                                                   len, size, byte_cnt);
             byte_cnt = 0;
-
+        }
+        /* Release held RPMsg rx buffer */
+        result = rpmsg_queue_nocopy_free(my_rpmsg, rx_buf);
+        if (result != 0)
+        {
+            assert(false);
         }
     }
 }
